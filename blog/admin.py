ca7fd80111c70e8e83f2899a1c7ee1594d99dee8
Akshay Das <akshay98322@gmail.com>
2020-06-24 20:50:37 +0530

    This is test commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Blogs
-# Register your models here.
+
 
 #whatever should be displayed in Admin page UI need to configure here
 admin.site.register(Blogs)
