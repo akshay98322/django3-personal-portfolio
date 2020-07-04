870de81e8c301f72201ef2db4623fa4972026b38
Akshay Das <akshay98322@gmail.com>
2020-07-04 12:11:12 +0530

    New project added, new Blog Added

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Project
-# Register your models here.
+
 
 #whatever should be displayed in Admin page UI need to configure here
 admin.site.register(Project)
